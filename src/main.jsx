4093aa41e69d5af019493e017be0168a1e493235
Alonso <donayrealonso987@gmail.com>
2026-05-27 02:00:00 -0600

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,15 @@
 import { StrictMode } from 'react';
-
 import { createRoot } from 'react-dom/client';
-
-// Importamos al componente padre
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-// Importación de bootstrap 
+//  Primero  CSS base
+import './index.css';
+
+// Bootstrap DESPUÉS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
-// Importación del diseño con css
-import './index.css';
-
-// Importacion para cambiar de pagina
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
